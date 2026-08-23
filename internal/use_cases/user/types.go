0e26df44dd632df9872468fd9c661236a1ec0304
egkogladyshev <egkogladyshev@edu.hse.ru>
2026-08-23 22:52:24 +0500

    Fixed DTO userRequest (added field Password)

--- internal/use_cases/user/types.go
+++ internal/use_cases/user/types.go
@@ -3,6 +3,7 @@ package user
 type userRequest struct {
 	Name         string `json:"name"`
 	Email        string `json:"email"`
+	Password     string `json:"password"`
 	PasswordHash string `json:"-"`
 }
 
